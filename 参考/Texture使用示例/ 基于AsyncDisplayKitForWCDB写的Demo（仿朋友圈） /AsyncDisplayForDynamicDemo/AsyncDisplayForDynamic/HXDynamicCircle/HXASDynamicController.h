3ea8ed0eff4cbbd77c5b1af9ae9eb5aa75bb4f1e
zhuyuhui <zhuyuhui@odianyun.com>
2023-09-21 20:35:14 +0800

    feat: [DSDemo-OC]~~~

---
+++ 参考/Texture使用示例/ 基于AsyncDisplayKitForWCDB写的Demo（仿朋友圈） /AsyncDisplayForDynamicDemo/AsyncDisplayForDynamic/HXDynamicCircle/HXASDynamicController.h
@@ -0,0 +1,14 @@
+//
+//  HXASDynamicController.h
+//  HongXun
+//
+//  Created by 张炯 on 2018/4/3.
+//
+
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
+
+@interface HXASDynamicController : ASDKViewController
+
+@property (nonatomic, assign) BOOL extendedTabBarInset; // 扩展到底部tabbar
+
+@end
